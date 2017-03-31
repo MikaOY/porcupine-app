c0825a9f93739d562440e2cd2cfad27af093564b
omnikitty <omnikitty.powers@gmail.com>
2017-03-31 16:45:41 +0800

    Added sexy todo page

--- Porcupine/TodoPage.xaml.cs
+++ Porcupine/TodoPage.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Diagnostics;
 using Xamarin.Forms;
 
 namespace Porcupine
@@ -10,6 +10,28 @@ namespace Porcupine
 		public TodoPage()
 		{
 			InitializeComponent();
+
 		}
+
+		bool triggered;
+
+		void RevealMenu(object sender, EventArgs e)
+		{
+			sideMenu.TranslateTo(sideMenu.Width, 0, 300, Easing.CubicInOut);
+			toggleButton.IsVisible = false;
+			triggered = true;
+		}
+
+		void HideMenu(object sender, EventArgs e)
+		{
+			if (triggered)
+			{
+				sideMenu.TranslateTo(-sideMenu.Width, 0, 300, Easing.CubicIn);
+				toggleButton.IsVisible = true;
+				triggered = false;
+			}
+		}
+
+
 	}
 }
